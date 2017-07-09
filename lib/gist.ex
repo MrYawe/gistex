ca8ceac2b1e70b15a24c96e678910f68f3431c1d
MrYawe <yannis.weishaupt@gmail.com>
2017-07-10 00:39:57 +0200

    Add all endpoints

--- lib/gist.ex
+++ lib/gist.ex
@@ -28,15 +28,59 @@ defmodule Gist do
 
   def process_request_body(body), do: Poison.encode!(body)
 
-  def list(user) do
+  def list!(user) do
     get!("/users/#{user}/gists").body
   end
 
-  def create(content, filename \\ "a.ex", options \\ []) do
-    create_multiple(%{filename => content}, options)
+  def list_all!(options \\ []) do
+    get!("/gists/public", [], params: options).body
   end
 
-  def create_multiple(files, options \\ []) do
+  def get!(id) do
+    get!("/gists/#{id}").body
+  end
+
+  def get!(id, sha) do
+    get!("/gists/#{id}/#{sha}").body
+  end
+
+  def create!(content, filename \\ "a.ex", options \\ []) do
+    create_multiple!(%{filename => content}, options)
+  end
+
+  def create_multiple!(files, options \\ []) do
     post!("/gists", Enum.into(options, %{files: files})).body
+  end
+
+  def edit!(id, content, filename \\ "a.ex", options \\ []) do
+    edit_multiple!(id, %{filename => content}, options)
+  end
+
+  def edit_multiple!(id, files, options \\ []) do
+    post!("/gists/#{id}", Enum.into(options, %{files: files})).body
+  end
+
+  def list_commits!(id) do
+    get!("/gists/#{id}/commits").body
+  end
+
+  def star!(id) do
+    put!("/gists/#{id}/star").body
+  end
+
+  def unstar!(id) do
+    delete!("/gists/#{id}/star").body
+  end
+
+  def fork!(id) do
+    post!("/gists/#{id}/forks", nil).body
+  end
+
+  def list_forks!(id) do
+    get!("/gists/#{id}/forks").body
+  end
+
+  def delete!(id) do
+    delete!("/gists/#{id}").body
   end
 end
